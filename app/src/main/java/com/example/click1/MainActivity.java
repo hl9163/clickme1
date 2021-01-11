5d842bb7753e97016124e8b9db1534e18a11c223
HAREL <hl9163@bs.amalnet.k12.il>
2021-01-11 22:24:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/click1/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.click1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void go(View view) {
+        Button bat = (Button) findViewById(R.id.button3);
+        bat.setText("Oh, yea, I’ve been clicked!");
+    }
+}
